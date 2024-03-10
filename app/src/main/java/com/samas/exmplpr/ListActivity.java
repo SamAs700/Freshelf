8cabc8ed06b0731432811370e55cfbc321be6df2
Sam As <sonis.artem19@gmail.com>
2024-03-10 18:25:33 +0300

    Added onItemClick dialog window
    (cherry picked from commit a3bade7e56b404af3661ae366c998595e593b437)

--- app/src/main/java/com/samas/exmplpr/ListActivity.java
+++ app/src/main/java/com/samas/exmplpr/ListActivity.java
@@ -1,9 +1,15 @@
 package com.samas.exmplpr;
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
@@ -12,18 +18,62 @@ import java.util.List;
 public class ListActivity extends AppCompatActivity {
 
     private ListView listView;
+    private AppDatabase database;
+    private ProductDao productDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppDatabase database = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "database").allowMainThreadQueries().build();
-        ProductDao productDao = database.productDao();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list1);
+
+        // Initialize the database and DAO
+        database = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database").allowMainThreadQueries().build();
+        productDao = database.productDao();
+
         listView = findViewById(R.id.listView);
         List<Product> productList;
         productList = productDao.getAll();
         ArrayAdapter<Product> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, productList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final Product product = (Product) parent.getItemAtPosition(position);
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+                builder.setTitle("Product Details")
+                        .setMessage(product.getName() + "\n" + product.getExpirationDate());
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(ListActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ProductDeleteAsyncTask task = new ProductDeleteAsyncTask(productDao);
+                        task.execute(product);
+                        productList.remove(product);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(ListActivity.this, "Product deleted", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+    private static class ProductDeleteAsyncTask extends AsyncTask<Product, Void, Void> {
+        private ProductDao mAsyncTaskDao;
+
+        ProductDeleteAsyncTask(ProductDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Product... products) {
+            mAsyncTaskDao.delete(products[0]);
+            return null;
+        }
     }
 }
