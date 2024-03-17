6d346279b84264a0338ac5acd7c10a326086db87
Sam As <sonis.artem19@gmail.com>
2024-03-17 18:00:04 +0300

    Localization

--- app/src/main/java/com/samas/exmplpr/ListActivity.java
+++ app/src/main/java/com/samas/exmplpr/ListActivity.java
@@ -1,6 +1,8 @@
 package com.samas.exmplpr;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -21,6 +24,7 @@ public class ListActivity extends AppCompatActivity {
     private ListView listView;
     private AppDatabase database;
     private ProductDao productDao;
+    private TextView userView;
     private Button backButton;
 
     @Override
@@ -32,6 +36,10 @@ public class ListActivity extends AppCompatActivity {
         productDao = database.productDao();
         listView = findViewById(R.id.listView);
         backButton = findViewById(R.id.backButton);
+        userView = findViewById(R.id.userView);
+        SharedPreferences sp = getSharedPreferences("hasVisited",
+                Context.MODE_PRIVATE);
+        userView.setText(sp.getString("username", "Имя"));
         List<Product> productList;
         productList = productDao.getAll();
         ArrayAdapter<Product> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, productList);
@@ -41,21 +49,21 @@ public class ListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 final Product product = (Product) parent.getItemAtPosition(position);
                 AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
-                builder.setTitle("Product Details")
+                builder.setTitle("Информация о продукте")
                         .setMessage(product.getName() + "\n" + product.getExpirationDate());
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                builder.setPositiveButton("ОК", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Toast.makeText(ListActivity.this, "OK", Toast.LENGTH_SHORT).show();
+
                     }
                 });
-                builder.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                builder.setNegativeButton("УДАЛИТЬ", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         ProductDeleteAsyncTask task = new ProductDeleteAsyncTask(productDao);
                         task.execute(product);
                         productList.remove(product);
                         adapter.notifyDataSetChanged();
-                        Toast.makeText(ListActivity.this, "Product deleted", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ListActivity.this, "Продукт удалён", Toast.LENGTH_SHORT).show();
                     }
                 });
                 AlertDialog dialog = builder.create();
